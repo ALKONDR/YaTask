c1bb40987a343ceb94257fd55cf9200ccff0828d
ALKONDR <kondrashkin.98@mail.ru>
2017-04-04 20:59:09 +0300

    basic TripMaker's methods implemented

--- tripmaker.js
+++ tripmaker.js
@@ -1,3 +1,57 @@
 class TripMaker {
-	
+	constructor() {
+		this.tripCards = [];
+		this.MUST_HAVE_PROPERTIES = ['from', 'to', 'transport'];
+		this.DEFAULT_VALUE_FOR_UNDEFINED = 'not assigned';
+	}
+
+	//method for quick finding intersection in 2 arrays
+	findIntersection(array1, array2) {
+		const tmp = {};
+		const intersection = [];
+
+		array1.forEach(element => {
+			tmp[element] = true;
+		});
+
+		array2.forEach(element => {
+			if (tmp[element])
+				intersection.push(element);
+		});
+
+		return intersection;
+	}
+
+	checkForValidTrip(trip) {
+		trip.forEach(card => {
+			if (!this.checkForValidCard(card))
+				return false;
+		});
+	}
+
+	checkForValidCard(card) {
+		if (this.findIntersection(this.MUST_HAVE_PROPERTIES, Object.keys(card)).length === this.MUST_HAVE_PROPERTIES.length)
+			return true;
+
+		return false;;
+	}
+
+	addCard(card) {
+		if(!this.checkForValidCard(card))
+			throw new Error('Missed some must have properties!');
+		this.tripCards.push(card);
+	}
+
+	clearTripCards() {
+		this.tripCards = [];
+	}
+
+	static createTrip(cards) {
+
+	}
 }
+
+const trip = new TripMaker();
+trip.addCard({from: 'Saratov', to: 'Moscow', transport: 'train'});
+trip.addCard({from: 'Moscow', to: 'Odintsovo', transport: 'bus'});
+console.log(trip.tripCards);
