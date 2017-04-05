f38c19147f3db1cc0c262ab20d495b8197886909
ALKONDR <kondrashkin.98@mail.ru>
2017-04-05 22:44:06 +0300

    some tests added

--- task2/test/test.js
+++ task2/test/test.js
@@ -1,14 +1,3 @@
-// const mlfEl = first('.testClassName').addClass('someClass');
-// console.dir(mlfEl.dom);
-
-
-// mlfEl.removeClass('testClassName').addClass('oneMoreClass');
-// console.log(mlfEl.dom);
-
-// console.log(mlfEl.hasClass('meClass'));
-
-// mlfEl.setText('some text');
-
 // mlfEl.css({
 // 	background: 'yellow',
 // 	height: '100px'
@@ -21,8 +10,52 @@
 // console.log(anotherEl.dom);
 
 // anotherEl.setText('another text');
+
+const htmlToAdd = `<div class="testClassName"></div> 
+
+  <div class="anotherClassName"></div>
+
+  <div class="oneClass twoClass threeClass"></div>
+
+  <div class="multipleElements"></div>
+  <div class="multipleElements"></div>
+  <div class="multipleElements"></div>
+  <div class="multipleElements"></div>
+  <div class="multipleElements"></div>
+
+  <script src='myLittleFramework.js'></script>
+  <script src='examples.js'></script>`;
+
+document.body.innerHTML = htmlToAdd;
+
 describe('MLF', function() {
-  it('should work', function() {
-    assert.equal(1, 1);
+  describe('addClass', function() {
+    it('should add class', function() {
+      assert.equal(first('.testClassName').addClass('someClass').dom.className, 'testClassName someClass');
+    });
+  });
+});
+
+describe('MLF', function() {
+  describe('removeClass', function() {
+    it('should remove class', function() {
+      assert.equal(first('.testClassName').removeClass('testClassName').dom.className, 'someClass');
+    });
   })
-})
+});
+
+describe('MLF', function() {
+  describe('hasClass', function() {
+    it('should be true', function() {
+      assert.equal(first('.someClass').hasClass('someClass'), true);
+    });
+  })
+});
+
+describe('MLF', function() {
+  describe('setText', function() {
+    it('should set text in the element', function() {
+      assert.equal(first('.someClass').setText('some text').dom.textContent, 'some text');
+    });
+  });
+});
