9e44dc6e5091a80ba81640ee3db5da89e8bdc955
ALKONDR <kondrashkin.98@mail.ru>
2017-04-05 17:46:36 +0300

    examples separated

--- task2/myLittleFramework.js
+++ task2/myLittleFramework.js
@@ -80,37 +80,8 @@ class MLF {
 	}
 }
 
+// returns first element with given selector
 const first = (selector) => new MLF(document.querySelector(selector));
 
+// returns all elements with given selectors
 const all = (selector) => new MLF(document.querySelectorAll(selector));
-
-
-
-//************************************************
-//--------------------TESTING---------------------
-//************************************************
-
-
-const mlfEl = first('.testClassName').addClass('someClass');
-console.dir(mlfEl.dom);
-
-
-mlfEl.removeClass('testClassName').addClass('oneMoreClass');
-console.log(mlfEl.dom);
-
-console.log(mlfEl.hasClass('meClass'));
-
-mlfEl.setText('some text');
-
-mlfEl.css({
-	background: 'yellow',
-	height: '100px'
-});
-
-mlfEl.addListener('onclick', () => console.log('onclick listener!'));
-
-const anotherEl = all('.multipleElements');
-anotherEl.toggleClass('superClass');
-console.log(anotherEl.dom);
-
-anotherEl.setText('another text');
