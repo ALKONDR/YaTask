7075edb8b4ccddb20646693288aff0fcaec2c065
ALKONDR <kondrashkin.98@mail.ru>
2017-04-05 21:38:13 +0300

    config tests added

---
+++ task2/gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const Server = require('karma').Server;
+const eslint = require('gulp-eslint');
+ 
+gulp.task('lint', () => {
+  return gulp.src(['src/*.js', '!node_modules/**'])
+    .pipe(eslint({
+      useEslintrc: true
+    }))
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('test', function (done) {
+  new Server({
+  configFile: __dirname + '/karma.conf.js',
+  singleRun: true,
+  }, done).start();
+});
+
+gulp.task('testwatch', function (done) {
+  new Server({
+  configFile: __dirname + '/karma.conf.js'
+  }, done).start();
+});
+
+gulp.task('default', ['lint', 'testwatch']);
